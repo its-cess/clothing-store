ea91f8c54da3e49ca73e0e92f61fed21f6912638
Cecily Toro <torocecily@gmail.com>
2022-10-06 14:45:15 -0400

    fix styling. add category titles

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,15 +1,15 @@
 import "./directory.styles.scss";
 
-import CatergoryItem from "../category-item/category-item.component";
+import DirectoryItem from "../directory-item.component.jsx/directory-item.component";
 
-const Directory = ({categories}) => {
+const Directory = ({ categories }) => {
   return (
     <div className="directory-container">
       {categories.map((category) => (
-        <CatergoryItem key={category.id} category={category} />
+        <DirectoryItem key={category.id} category={category} />
       ))}
     </div>
-  )
-}
+  );
+};
 
 export default Directory;
