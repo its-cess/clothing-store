41a91191ef05d474c73649bbdb95aed2accedb57
Cecily Toro <torocecily@gmail.com>
2022-10-07 10:34:40 -0400

    change scss files to styled components

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,9 +1,16 @@
 import { useContext } from "react";
 import { CartContext } from "../../contexts/cart.context";
 
-import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
+import { BUTTON_TYPE_CLASSES } from "../button/button.component";
 
-import "./product-card.styles.scss";
+import {
+  ProductCardContainer,
+  ProductCardFooter,
+  ProductName,
+  ProductPrice,
+  ProductCardButton,
+  ProductImage
+} from "./product-card.styles";
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
@@ -12,19 +19,19 @@ const ProductCard = ({ product }) => {
   const addProductToCart = () => addItemToCart(product);
 
   return (
-    <div className="product-card-container">
-      <img src={imageUrl} alt={`${name}`} />
-      <div className="footer">
-        <span className="name">{name}</span>
-        <span className="price">{price}</span>
-      </div>
-      <Button
+    <ProductCardContainer>
+      <ProductImage src={imageUrl} alt={`${name}`} />
+      <ProductCardFooter>
+        <ProductName>{name}</ProductName>
+        <ProductPrice>{price}</ProductPrice>
+      </ProductCardFooter>
+      <ProductCardButton
         buttonType={BUTTON_TYPE_CLASSES.inverted}
         onClick={addProductToCart}
       >
         Add to cart
-      </Button>
-    </div>
+      </ProductCardButton>
+    </ProductCardContainer>
   );
 };
 
