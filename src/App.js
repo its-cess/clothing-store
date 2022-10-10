90624c45288c89f52ccef67ae3b201499a6ed448
Cecily Toro <torocecily@gmail.com>
2022-10-10 14:18:30 -0400

    refactor to use redux for user and categories context

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
 import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import {
+  onAuthStateChangedListener,
+  createUserDocumentFromAuth
+} from "./utils/firebase/firebase.utils";
+
+import { setCurrentUser } from "./store/user/user.action";
 
 import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
@@ -7,6 +16,19 @@ import Shop from "./routes/shop/shop.component";
 import Checkout from "./components/checkout/checkout.component";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      dispatch(setCurrentUser(user));
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
