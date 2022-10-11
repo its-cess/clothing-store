d3d46982e2d93db90dfa3cfb1aea304bf91ffb68
Cecily Toro <torocecily@gmail.com>
2022-10-11 15:45:03 -0400

    migrate cart components from context to redux

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,6 +1,23 @@
-export const selectCategoriesMap = (state) =>
-  state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => state.categories;
+
+//create memoized selector so it only runs if the categories changes
+export const selectCategories = createSelector(
+  //first argument is an array of input selectors
+  //what "slice" of the store do you want
+  [selectCategoryReducer],
+  //second argument is an output. whatever the input selector gets back,
+  //that will be what the output function receives.
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) =>
+    categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+);
